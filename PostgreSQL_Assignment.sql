a252aea02c8a8c0aa58a39e788843b062c19f3bd
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-26 22:41:32 +0600

    all are problem solved

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -98,8 +98,81 @@ VALUES (
         '2024-05-18 18:30:00',
         NULL
     );
+
+
+
+-- problems - 01
 INSERT INTO
     rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
+
+-- problems - 02
 SELECT count(DISTINCT species_id) AS unique_species_count
 FROM sightings;
+
+-- problems - 03
+SELECT * FROM sightings WHERE location LIKE '%Pass%';
+
+-- problems - 04
+SELECT name, count(*) AS total_sightings
+FROM sightings
+    JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+GROUP BY
+    name
+ORDER BY name ASC;
+
+-- problems - 05
+SELECT common_name
+FROM species
+    LEFT JOIN sightings ON species.species_id = sightings.species_id
+WHERE
+    sightings.sighting_id is NULL;
+
+-- problems - 06
+SELECT common_name, sighting_time, name
+FROM
+    sightings
+    JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+    JOIN species ON sightings.species_id = species.species_id
+ORDER BY sighting_time DESC LIMIT 2;
+
+-- problems - 07
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    extract(
+        YEAR
+        FROM discovery_date
+    ) < 1800;
+
+-- problem - 08
+SELECT
+    sighting_id,
+    CASE
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 0 AND 11  THEN 'Morning'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 12 AND 17  THEN 'Afternoon'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) BETWEEN 18 AND 23  THEN 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+-- problem - 09
+DELETE FROM rangers
+WHERE
+    ranger_id IN (
+        SELECT rangers.ranger_id
+        FROM rangers
+            LEFT JOIN sightings ON rangers.ranger_id = sightings.ranger_id
+        WHERE
+            sightings.ranger_id IS NULL
+    );
+
